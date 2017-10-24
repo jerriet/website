b91428ffe0bd1ffe8cc5571f10a3c7b37c05eabf
Jerry Kieboom <jerrykieboom@waltertoshiba.msg.loc>
2017-10-24 17:43:39 +0200

    end of day

--- js/functions.js
+++ js/functions.js
@@ -3,7 +3,7 @@ function myName() {
     if (name.length < 9){
     if (name != null) {
         document.getElementById("setname").innerHTML = " Welcome " + name;
-        document.getElementById("loginbutton").innerHTML = "You are logged in </br> <a href='#pubgstats'>My PUBG Stats</a> </br> <a href='#rocketleaguestats'>My RocketLeague Stats</a> </br> <a href='#counterstrike'>My Counterstrike Stats</a> </br> <button onClick='window.location.reload()'>Uitloggen</button>";
+        document.getElementById("loginbutton").innerHTML = "<div class='list'> You are logged in </br> <a href='#pubgstats'>My PUBG Stats</a> </br> <a href='#rocketleaguestats'>My RocketLeague Stats</a> </br> <a href='#counterstrike'>My Counterstrike Stats</a> </br><element class='logout'> <button value='login' type='submit' onClick='window.location.reload()'>Uitloggen</button></element></div>";
         } 
     } else {
         alert("Username too long (max 8 characters)!");
